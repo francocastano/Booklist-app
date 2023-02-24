f9ae5e96d27995b046bd4b6c4ed2e6e53f976bdb
Franco-Castaño <francomatiascastano@gmail.com>
2023-02-23 23:42:08 -0300

    Comit-desde.notebook

--- js/app.js
+++ js/app.js
@@ -7,30 +7,49 @@ class Book{
     }
 }
 
+
 // UI Class: Handle UI Tasks
 class UI{
     static displayBooks() {
+        const StoredBooks = [
+            {
+                title: 'Book One',
+                author: 'john Doe',
+                isbn: '3434434'
+            }
+        ];
 
-            const books = Store.getBooks();
-            books.forEach((book) => UI.addBookToList(book));
-    }
 
-    static addBookToList(book){
+        const books = Store.getBooks();
+        books.forEach((book) => UI.addBookToList(book));
+            
+        static addBookToList(book){
         const list = document.querySelector('#book-list');
         
         const row = document.createElement('tr');
 
-        row.innerhtml =`
+        row.innerHTML =`
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.isbn}</td>
         <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
         `;
 
-        list.appendChild(row);
-    }
+        const books = Store.getBooks ();
+        books.forEach((book) => UI.addBookToList(book));
+        
 
-    static deleteBookFromList(el){
+        list.appendChild(row);
+
+        static clearFields(){
+            document.querySelector('title').value = '';
+            document.querySelector('author').value = '';
+            document.querySelector('isbn').value = '';
+        }
+    }
+}
+    
+    static deleteBookFromList(e.target){
         if(el.classList.contains('delete')){
             el.parentElement.parentElement.remove();
         }
